3d2eedc5e73a69d1c89336f9d8ec95a909c73712
TairilKK <j.dobrosielski4@gmail.com>
2023-04-25 17:02:43 +0200

    9. Zastosowanie mniejszej widoczosci modyfikujac public na private

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-    int totalrejected = 0;
+    private int totalrejected = 0;
 
     public int reportRejected() {
         return totalrejected;
--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,15 +2,15 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    public static final int EMPTY_RHYMER_INDICATOR = -1;
-    public static final int DEFAULT_VALUE = -1;
+    private static final int EMPTY_RHYMER_INDICATOR = -1;
+    private static final int DEFAULT_VALUE = -1;
 
 
-    public static final int MAX_SIZE = 12;
-    public static final int CAPACITY = MAX_SIZE -1;
+    private static final int MAX_SIZE = 12;
+    private static final int CAPACITY = MAX_SIZE -1;
     private final int[] numbers = new int[MAX_SIZE];
 
-    public int total = EMPTY_RHYMER_INDICATOR;
+    private int total = EMPTY_RHYMER_INDICATOR;
 
     public void countIn(int in) {
         if (!isFull())
